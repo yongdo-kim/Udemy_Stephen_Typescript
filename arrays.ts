916a7495cb21f441e1777fe8b3f4f56f4ae4cbbe
yongdo-kim <nbmg1128@gmail.com>
2023-05-17 22:32:03 +0900

    feat feature/5

---
+++ arrays.ts
@@ -0,0 +1,25 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map(
+  (car: string): string => {
+    return car.toUpperCase();
+  }
+);
+
+// Flexible types
+
+//유니온인데, 배열인 상황.
+const importantDates: (Date | string)[] = [];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
